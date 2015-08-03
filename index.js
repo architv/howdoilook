e50da2b36b48ff6763d5c408dc366c992eb79f2e
Archit Verma <architv07@gmail.com>
2015-08-03 08:11:51 +0530

    added google analytics; option to submit suggestions

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 window.onload = function() { init() };
 
 	var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1j8-PvfpixsB2URW8PcvornQbRzIA4YMNu2GxEwvag7M/pubhtml';
+	var previousIndex = -1;
 
 	function init() {
-	commentData = Tabletop.init({ key: public_spreadsheet_url, callback:function(data) {console.log(data)}, simpleSheet: true })
+		commentData = Tabletop.init({ key: public_spreadsheet_url, callback:function(data) {console.log(data)}, simpleSheet: true })
 	}
 
 	$(function() {      
       $(".wrapper").swipe( {
         //Generic swipe handler for all directions
         tap:function(event, target) {
-          // $(this).text("You swiped " + direction ); 
-			var index = Math.floor(Math.random() * (commentData.data().length));
-			clickAdvice(index);
+        	var index = getRandomNumber(commentData.data().length);
+        	while (index == previousIndex) {
+        		index = getRandomNumber(commentData.data().length);
+        	}
+        	previousIndex = index;
+			getSuggestion(index);
         }
       });
     });
 
-	function showInfo(data, tabletop) {
-		// alert("Successfully processed!")
-		comment_data = data;
-		var comment = document.getElementById("comment");
-		var index = Math.floor(Math.random() * (data.length));
-		comment.innerText = data[index]["Comment"];
-		comment.className = comment.className + " animated bounce title";
-		console.log(data);
+	function getRandomNumber(max) {
+		return Math.floor(Math.random() * (max));
 	}
 
-	function clickAdvice(index) {
+	function getSuggestion(index) {
 	    // console.log(randNum);
 	    $("#comment").removeClass().html(commentData.data()[index].Comment).addClass('animated flipInX title').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 	      $(this).removeClass().addClass('title');
@@ -39,5 +37,5 @@ window.onload = function() { init() };
 	    } else {
 	    	$("#credits").html("");
 	    }
-	    console.log("inside clickAdvice");
+	    console.log("inside getSuggestion");
 	} 
